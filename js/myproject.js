5704fc463aed3dd35f205af70adad3e08a66f132
matuzau <matuzov@yahoo.com>
2018-09-27 00:05:50 +0300

    add contact page and some js funct

--- js/myproject.js
+++ js/myproject.js
@@ -6,8 +6,9 @@ $(function() {
         $(this).next('.list-link').toggleClass('open')
 
     });
+});
 
-    $('#slider-price').slider({
+/*$('#slider-price').slider({
         max: 40,
         min: 0,
         range: true,
@@ -19,7 +20,38 @@ $(function() {
     });
 
     $('input[name="minPrice"]').val(
-        $('#slider-price').slider('values', 0));
+        '$' + $('#slider-price').slider('values', 0));
     $('input[name="maxPrice"]').val(
-        $('#slider-price').slider('values', 1));
-})
+        'S' + $('#slider-price').slider('values', 1));
+
+
+    $('#cart').click(function() {
+        $('.cart-box').toggleClass('open');
+    });
+
+
+});*/
+
+autoSlider();
+var left = 0;
+var timer;
+
+
+
+function autoSlider() {
+    timer = setTimeout(function() {
+        var banner = document.getElementById('bannerSlider');
+        left = left - 1370;
+        if (left < -4110) {
+            left = 0;
+            clearTimeout(timer);
+        }
+        banner.style.left = left + 'px';
+        autoSlider();
+    }, 3000);
+};
+
+function show(state) {
+    document.getElementById('window').style.display = state;
+    document.getElementById('gray').style.display = state;
+}
