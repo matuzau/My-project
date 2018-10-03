f336db0504e972d42869dab6bb05c327ff9f7e44
matuzau <matuzov@yahoo.com>
2018-10-03 23:37:18 +0300

    add Java Script on goods

--- js/myproject.js
+++ js/myproject.js
@@ -8,134 +8,105 @@ $(function() {
     });
 });
 
-/*$('#slider-price').slider({
-        max: 40,
-        min: 0,
-        range: true,
-        values: [3, 30],
-        slide: function(event, ui) {
-            $('.input[name="minPrice"]').val(ui.values[0]);
-            $('.input[name="maxPrice"]').val(ui.values[1]);
-        }
-    });
-
-    $('input[name="minPrice"]').val(
-        '$' + $('#slider-price').slider('values', 0));
-    $('input[name="maxPrice"]').val(
-        'S' + $('#slider-price').slider('values', 1));
-
-
-    $('#cart').click(function() {
-        $('.cart-box').toggleClass('open');
-    });
-
-
-});*/
-
-// autoSlider();
-// var left = 0;
-// var timer;
-
-
-
-// function autoSlider() {
-//     timer = setTimeout(function() {
-//         var banner = document.getElementById('bannerSlider');
-//         left = left - 1140;
-//         if (left < -3420) {
-//             left = 0;
-//             clearTimeout(timer);
-//         }
-//         banner.style.left = left + 'px';
-//         autoSlider();
-//     }, 3000);
-// };
 
 function show(state) {
     document.getElementById('window').style.display = state;
     document.getElementById('gray').style.display = state;
 };
 
-var goods = {
-    "item1": {
-        "name": "Клей Тайфун №18",
-        "cost": 8.00,
-        "description": "Клей для блоков",
-        "image": "images/клей18.jpg"
+// var cartBtn = document.querySelector('#cart');
+// var cartOpen = document.querySelector('.cart-box')
+// cartBtn.onclick = function() {
+//     cartOpen.className = 'open';
+// };
+$('#cart').click(function() {
+    $('.cart-box').toggleClass('open');
+});
+
+// var navlinks = document.querySelector('.header__nav li #nav-links');
+// navlinks.onfocus = function() {
+//     navlinks.style.backgroundColor = 'red';
+// };
+
+var goods = [{
+        name: "Клей Тайфун №18",
+        cost: 8.00,
+        description: "Клей для блоков",
+        image: "images/клей18.jpg"
     },
-    "item2": {
-        "name": "Клей Тайфун №50",
-        "cost": 9.50,
-        "description": "Клей для приклеивания утеплителя",
-        "image": "images/клей50.jpg"
+    {
+        name: "Клей Тайфун №50",
+        cost: 9.50,
+        description: "Клей для приклеивания утеплителя",
+        image: "images/клей50.jpg"
     },
-    "item3": {
-        "name": "Штукатурка Тайфун №22",
-        "cost": 14.70,
-        "description": "Декоративная штукатурка",
-        "image": "images/тайфун22.jpg"
+    {
+        name: "Штукатурка Тайфун №22",
+        cost: 14.70,
+        description: "Декоративная штукатурка",
+        image: "images/тайфун22.jpg"
     },
-    "item4": {
-        "name": "Клей Люкс",
-        "cost": 5.40,
-        "description": "Клей для плитки",
-        "image": "images/клейлюкс.jpg"
+    {
+        name: "Клей Люкс",
+        cost: 5.40,
+        description: "Клей для плитки",
+        image: "images/клейлюкс.jpg"
     },
-    "item5": {
-        "name": "Белгипс ГКЛ",
-        "cost": 11.80,
-        "description": "Гипсокартон для стен",
-        "image": "images/белгипсгкл.jpg"
+    {
+        name: "Белгипс ГКЛ",
+        cost: 11.80,
+        description: "Гипсокартон для стен",
+        image: "images/белгипсгкл.png"
     },
-    "item6": {
-        "name": "Кнауф ГКЛВО",
-        "cost": 13.10,
-        "description": "Гипсокартон для стен",
-        "image": "images/кнауфгкло.jpg"
+    {
+        name: "Кнауф ГКЛВО",
+        cost: 13.10,
+        description: "Гипсокартон для стен",
+        image: "images/кнауфгкло.jpg"
     },
-    "item7": {
-        "name": "Клей Тайфун №51",
-        "cost": 9.60,
-        "description": "Клей для утеплителя",
-        "image": "images/клей51.jpg"
+    {
+        name: "Клей Тайфун №51",
+        cost: 9.60,
+        description: "Клей для утеплителя",
+        image: "images/клей51.jpg"
     },
-    "item8": {
-        "name": "Белгипс ГКЛВ",
-        "cost": 10.50,
-        "description": "Гипсокартон для стен",
-        "image": "images/белгипсгклв.jpg"
+    {
+        name: "Белгипс ГКЛВ",
+        cost: 10.50,
+        description: "Гипсокартон для стен",
+        image: "images/белгипсгклв.png"
     },
-    "item9": {
-        "name": "Стяжка Тайфун №42",
-        "cost": 14.70,
-        "description": "Стяжка для пола",
-        "image": "images/тайфун42.jpg"
+    {
+        name: "Стяжка Тайфун №42",
+        cost: 14.70,
+        description: "Стяжка для пола",
+        image: "images/тайфун42.jpg"
     }
-};
-console.log(goods);
+];
 
-var out = '';
-for (var key in goods) {
-    out += 'Название: ' + goods[key].name + '<br>';
-    out += 'Цена: ' + goods[key].cost + '<br>';
+var list = document.querySelector('.list-product');
+goods.forEach(function(item) {
+    var box = createBox(item);
+    list.appendChild(box);
+});
+
+function createBox(item) {
+    var box = document.createElement('div');
+    box.classList.add("product");
+    box.innerHTML =
+        '<div class="product-img"><img src=' + item.image + '></div>' +
+        '<div class="product-title">' + item.name + '</div>' +
+        '<div class="product-description">' + item.description + '</div>' +
+        '<div class="product-price">' + item.cost + " руб " + '</div>' +
+        '<button class="product-btn">' + "Купить" + '</button>'
+    return box;
 }
-document.getElementById('out').innerHTML = out;
-// $('document').ready(function() {
-//     loadGoods();
-
-// });
-
-// function loadGoods() {
-//     $.getJSON('goods.json', function(data) {
-//         var out = '';
-//         for (var key in data) {
-//             out += '<div class="single-goods">';
-//             out += '<h3>' + data[key]['name'] + '</h3>';
-//             out += '<p>Цена: ' + data[key]['cost'] + '</p>';
-//             out += '<img src="' + data[key].image + '">';
-//             out += '<button>Купить</button>';
-//             out += '</div>';
-//         }
-//         $('#goods').html(out);
-//     });
-// }
+// var product = document.querySelectorAll('#product-title');
+// // var img = document.createElement('img');
+// var h2 = document.createElement('h2');
+// var name = item.name;
+// h2.innerText = name;
+// h2.innerHTML = name;
+// // img.innerHTML = '<img src="' + item.image + '">';
+// product[1].appendChild(h2);
+// console.log(h2);
